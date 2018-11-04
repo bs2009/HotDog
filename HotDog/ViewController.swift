ea5f5f6e9aadb650b4828969ce9a314290800c95
Bill Song <bsong2009@gmail.com>
2018-11-04 12:39:54 -0600

    Initial Commit

---
+++ HotDog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotDog
+//
+//  Created by William Song on 11/4/18.
+//  Copyright © 2018 William Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
